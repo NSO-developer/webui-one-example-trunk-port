4ff5449e0b9a6a305b6022bdb4f3571a4dfbb73d
reriksso <robert.eriksson@dataductus.se>
2018-02-01 09:41:55 +0100

    Add info regarding netsim

---
+++ README.md
@@ -0,0 +1,50 @@
+# NSO example package, simple webui to create a trunk port
+
+## Purpose
+Example-trunk-port NSO package demonstrates how the JSON-RPC API and a simple webui can be used in combination with NSO webui-one to create custom applications.
+* This package will set up a simple trunk between two swithces.
+* The package is based on a NSO service template.
+* This application is based on a template for use with Cisco ios devices.
+
+## Package contents
+* __src/yang/example-trunk-port.yang__ Yang service definition.
+* __templates/example-trunk-port-template.xml__ Config template for Cisco ios device used with the service.
+* __webui/webui.json__ Required for the package to be recognised by the NSO webui and a link to the package webui will be visible on the ApplicationHUB.
+* __webui/index.html__ Required entrypoint for the package webui, where the user will end up when clicking on the link in the ApplicationHUB.
+* __webui/script.js__ Simple Javascript to peform a request to the NSO JSON-RPC.
+* __webui/style.css__ CSS Styles.
+
+## Requirements
+* NSO version 4.5+
+* A modern version of Chrome or Firefox
+* cisco-ios ned package
+  * Available in NSO-install/packages/neds/cisco-ios, copy to your NSO/packages folder.
+  * Run _make_ in the copied cisco-ios/src folder.
+* At least 1 Cisco device, 2 for a more authentic example. Real device or netsim.
+  * For netsim device look in NSO User Guide, chapter "The Network Simulator"
+
+## Installation
+__Download and set up__
+* Download and put the package contents in the NSO `packages/example-trunk-port` folder.
+* You have to source nso to reach nso commands like ncs_cli
+  * _source NSO-install-folder/nsoenv_
+* in `example-trunk-port/src/` run _make_
+
+__Reload packages using cli or webui.__
+* CLI: start ncs_cli
+  * run _request packages reload_
+* WEBUI: In your browser go to http://NSO-HOST/webui-one/ConfigurationEditor/ncs:packages/reload
+  * Click "Run reload action"
+
+__You have to restart NSO__
+* Restart NSO
+
+The example-webui should now be accessible from the ApplicationHUB, _nso-host/webui-one/_ or
+directly at _nso-host/example-trunk-port_
+
+## Usage
+Create a new trunk configuration:
+* Give the config a name "MyNewTrunk".
+* Fill in devices, ports and vlan-id.
+* Submit with "Create/Update Trunk from values" 
+* Go to the webui-one commit manager to see changes in NSO and Device config.
---
+++ webui/script.js
@@ -0,0 +1,175 @@
+//  Example request to NSO JSONRPC
+
+// jsonrpc request counter, each request should have an unique id.
+let id = 0;
+let xTh; // transaction handle
+
+// Return JSON representation of method and param
+const newRequest = (method, params) => {
+    id += 1;
+    return JSON.stringify({ jsonrpc: '2.0', id, method, params });
+};
+
+// JSON-RPC helper
+// Return request promise
+const jsonrpc = (method, params) => {
+    const url = `/jsonrpc/${method}`;
+    const body = newRequest(method, params);
+
+    return fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            Accept: 'application/json;charset=utf-8',
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body,
+    }).then((response) => {
+        if (response.status !== 200) {
+            return Promise.reject(new Error(method, response, body));
+        }
+        return response.json();
+    }).then((json) => {
+        if (json.error) {
+            return Promise.reject(new Error(json.error, body));
+        }
+        return json.result;
+    });
+};
+
+// Check if a read/write transaction exists
+const checkIfReadWriteTrans = () => (
+    jsonrpc('get_trans', {}).then((resp) => {
+        let res = false;
+        resp.trans.forEach((transItem) => {
+            if (transItem.mode === 'read_write') {
+                res = transItem.th;
+            }
+        });
+        return res;
+    })
+);
+
+// Crate a new read/write transaction
+const createNewTrans = () => (
+    jsonrpc('new_trans', {
+        db: 'running', mode: 'read_write', conf_mode: 'private' }).then(resp => resp.th)
+);
+
+// Get transaction id for existing or new transaction
+const getTransHandle = () => (
+    checkIfReadWriteTrans().then((existingTH) => {
+        if (existingTH) {
+            return existingTH;
+        }
+        return createNewTrans();
+    })
+);
+
+// Check if change already exsist before create
+const checkExists = (th, path) => (
+    jsonrpc('exists', { th, path }).then(resp => resp.exists)
+);
+
+// Create a new trunk port
+const createNew = (th, path) => (
+    checkExists(th, path).then((existsResp) => {
+        if (!existsResp) {
+            return jsonrpc('create', { th, path }).then((createResp) => {
+                if (JSON.stringify(createResp) === '{}') {
+                    return 'Transaction updated';
+                }
+                return `resp ${JSON.stringify(createResp)}`;
+            });
+        }
+        return 'The change already Exists!';
+    })
+);
+
+// Set a value
+const setValue = (th, path, value) => (
+    jsonrpc('set_value', { th, path, value })
+);
+
+// Set basePath to "example-trunk-port" service
+const basePath = '/example-trunk-port:example-trunk-port';
+
+// Create a new trunk instance
+const createNewTrunk = (th, trunkName) => {
+    const path = `${basePath}{${trunkName}}`;
+    return createNew(th, path);
+};
+
+// Delete trunk if exists (kind of update)
+const deleteTrunk = (th, trunkName) => {
+    const path = `${basePath}{${trunkName}}`;
+    return checkExists(th, path).then((existsResp) => {
+        if (existsResp) {
+            return jsonrpc('delete', { th, path });
+        }
+        return false;
+    });
+};
+
+// Set vlan id for trunk
+const setVlan = (th, trunkName, vlanId) => {
+    const path = `${basePath}{${trunkName}}/vlanid`;
+    return setValue(th, path, vlanId);
+};
+
+// Add a interface to device, device will be auto created when adding interface
+const addInterface = (th, trunkName, deviceName, interF) => {
+    const path = `${basePath}{${trunkName}}/device{${deviceName}}/interface{${interF}}`;
+    return createNew(th, path);
+};
+
+// Called on click in webui, execute create and set operations
+const doCreate = () => { // eslint-disable-line no-unused-vars
+    const trunkName = document.getElementById('trunkname').value;
+    const deviceA = document.getElementById('setDeviceA').value;
+    const interfaceA = document.getElementById('setInterfaceA').value;
+    const deviceB = document.getElementById('setDeviceB').value;
+    const interfaceB = document.getElementById('setInterfaceB').value;
+    const vlanId = document.getElementById('setVlan').value;
+
+    deleteTrunk(xTh, trunkName).then((res) => {
+        createNewTrunk(xTh, trunkName).then(() => {
+            setVlan(xTh, trunkName, vlanId).then(() => {
+                Promise.all([
+                    addInterface(xTh, trunkName, deviceA, interfaceA),
+                    addInterface(xTh, trunkName, deviceB, interfaceB),
+                ]).then(() => {
+                    if (res) {
+                        document.getElementById('result').innerText = 'Updated';
+                    } else {
+                        document.getElementById('result').innerText = 'Created';
+                    }
+                    location.reload();
+                });
+            });
+        });
+    });
+};
+
+// Get a list of existing trunks created with this service
+const getExistingTrunks = (th) => {
+    const path = '/example-trunk-port:example-trunk-port';
+    const resultAs = 'leaf_value_as_string';
+    const selection = ['name'];
+    jsonrpc('query', { th, path, result_as: resultAs, selection }).then((res) => {
+        const element = document.getElementById('trunks');
+        (res.results || []).forEach((trunk) => {
+            const opt = document.createElement('option');
+            opt.value = trunk;
+
+            element.appendChild(opt);
+        });
+    });
+};
+
+window.addEventListener('load', () => {
+    getTransHandle().then((res) => {
+        xTh = res;
+        getExistingTrunks(xTh);
+    });
+});
